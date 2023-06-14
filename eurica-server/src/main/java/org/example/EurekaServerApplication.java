a5966b01dcabb24fc3357bd2d97396188510ab3b
777 <goszakupki.aktiv@gmail.com>
2023-06-14 23:33:37 +0300

    start

---
+++ eurica-server/src/main/java/org/example/EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
